73fc6a0384ce2829494e46c8747d9dde686c9eb9
AlfredRenggiTay <alfredrenggitay@gmail.com>
2025-05-11 21:02:13 +0800

    Add files via upload

---
+++ uts-oop-2301010352/src/uts/oop/pkg2301010352/UtsOop2301010352.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package uts.oop.pkg2301010352;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class UtsOop2301010352 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
